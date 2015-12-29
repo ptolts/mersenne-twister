14c46c2a94c2b75ad0739dca8cadc2fcb523a2a8
Philip Tolton <mrtolton@gmail.com>
2015-12-29 13:48:49 -0500

    least significant bits

--- mersenne.rb
+++ mersenne.rb
@@ -15,6 +15,56 @@ class Fixnum
   end
 end
 
+F = 1812433253
+W = 32
+M = 397
+R = 31
+A = 2567483615
+U = 11
+D = 4294967295
+S = 7
+T = 15
+C = 4022730752
+B = 2636928640
+N = 624
+L = 18
+UPPER_MASK = (~1.generate_mask(W)).least_significant(W)
+
+class Mersenne
+  attr_accessor :index, :mt
+  def initialize(seed)
+    @index  = N
+    @mt     = Array.new(N, 0)
+    @mt[0]  = seed
+
+    (1..N).each do |i|
+      last_val          = @mt[i-1]
+      last_val_shifted  = last_val >> (W - 2)
+      xored             = last_val ^ last_val_shifted
+      @mt[i]            = (F * xored) + i
+    end
+  end
+
+  def extract_number
+    twist if @index >= N
+
+    y = @mt[@index]
+    y = y ^ ((y >> U) & D)
+    y = y ^ ((y << S) & B)
+    y = y ^ ((y << T) & C)
+    y = y ^ (y >> L)
+
+    @index += 1
+    y.least_significant(W)
+  end
+
+  def twist
+    (0..N).each do |i|
+
+    end
+  end
+end
+
 number = 100
 least_significant_bits = 6
 
