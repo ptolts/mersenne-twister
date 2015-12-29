d0420878a4aed7c8e3a7e81b75f54d20dd6b7d53
Philip Tolton <mrtolton@gmail.com>
2015-12-29 16:36:54 -0500

    Add references and image generator.

---
+++ README.md
@@ -0,0 +1,11 @@
+I wanted to learn more about PRNG code after reading [this article on the v8 PRNG](http://hackaday.com/2015/12/28/v8-javascript-fixes-horrible-random-number-generator/). I implemented a mersenne twister based on Wikipedia pseudocode. Definitely makes me appreciate how difficult crypto is.
+
+![alt text](/random.png 'Image generated using my mersenne code. Looks random eh?')
+
+#Links
+
+http://boallen.com/random-numbers.html
+
+https://www.random.org/analysis/
+
+https://en.wikipedia.org/wiki/Mersenne_Twister
--- mersenne.rb
+++ mersenne.rb
@@ -1,34 +1,39 @@
-class Fixnum
-  def to_binary
-    self.to_s(2)
-  end
-
-  def least_significant(n)
-    self & generate_mask(n)
-  end
-
-  def generate_mask(n)
-    (0).upto(n - 1).inject(0) do |result, index|
-      result += (1 << index)
-      result
-    end
-  end
-end
+require 'chunky_png'
 
 F = 1812433253
 W = 32
 M = 397
 R = 31
-A = 2567483615
+A = Integer('0x9908B0DF')
 U = 11
-D = 4294967295
+D = Integer('0xFFFFFFFF')
 S = 7
 T = 15
-C = 4022730752
-B = 2636928640
+C = Integer('0xEFC60000')
+B = Integer('0x9D2C5680')
 N = 624
 L = 18
-UPPER_MASK = (~1.generate_mask(W)).least_significant(W)
+LOWER_MASK = (1 << R) - 1
+LOWER_W_MASK = ((1 << W) - 1)
+UPPER_MASK = (~LOWER_MASK) & LOWER_W_MASK
+
+module ExtendNumber
+  def to_binary
+    self.to_s(2)
+  end
+
+  def least_significant
+    self & LOWER_W_MASK
+  end
+end
+
+class Fixnum
+  include ExtendNumber
+end
+
+class Bignum
+  include ExtendNumber
+end
 
 class Mersenne
   attr_accessor :index, :mt
@@ -37,16 +42,17 @@ class Mersenne
     @mt     = Array.new(N, 0)
     @mt[0]  = seed
 
-    (1..N).each do |i|
+    (1..(N - 1)).each do |i|
       last_val          = @mt[i-1]
       last_val_shifted  = last_val >> (W - 2)
       xored             = last_val ^ last_val_shifted
-      @mt[i]            = (F * xored) + i
+      multiplied        = (F * xored) + i
+      @mt[i]            = multiplied & LOWER_W_MASK
     end
   end
 
   def extract_number
-    twist if @index >= N
+    twist if @index == N
 
     y = @mt[@index]
     y = y ^ ((y >> U) & D)
@@ -55,21 +61,30 @@ class Mersenne
     y = y ^ (y >> L)
 
     @index += 1
-    y.least_significant(W)
+    y.least_significant
   end
+  alias :random :extract_number
 
   def twist
-    (0..N).each do |i|
-
+    (0..(N - 1)).each do |i|
+      x      = (@mt[i] & UPPER_MASK) +
+                (@mt[(i+1) % N] & LOWER_MASK)
+      xa     = x >> 1
+      xa     = xa ^ A if (x % 2) != 0
+      @mt[i] = @mt[(i + M) % N] ^ xa
     end
+    @index = 0
   end
 end
 
-number = 100
-least_significant_bits = 6
+mersenne = Mersenne.new(1)
+image_size = 512
 
-puts number.to_binary
+png = ChunkyPNG::Image.new(image_size, image_size, ChunkyPNG::Color::WHITE)
+(1..image_size - 1).each do |x|
+  (1..image_size - 1).each do |y|
+    png[x, y] = ChunkyPNG::Color.rgb(0, 0, 0) if (mersenne.random % 2 == 0)
+  end
+end
 
-puts 'mask: ', number.generate_mask(least_significant_bits).to_binary
-
-puts 'result: ', number.least_significant(least_significant_bits).to_binary
+png.save('random.png')
